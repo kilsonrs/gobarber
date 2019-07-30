483d5a2a0174ea0bba95629779896b261cf8451e
Kilson <kilsonrs@gmail.com>
2019-07-30 15:36:49 -0300

    configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '41e551b19f4637',
+    pass: '3e899bba166cf7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril(Mailchimp)
+
+// Mailtrap(DEV);
